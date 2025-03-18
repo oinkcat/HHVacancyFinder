820ee2a94a7fb9f669a737dbd9618e453f5be2d3
oinkcat <softcat@live.ru>
2025-03-18 13:53:50 +0300

    Добавление проекта модульных тестов

---
+++ HHVacancies.Tests/VacancyFinderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HHVacancies.Data;
+
+namespace HHVacancies.Tests
+{
+    /// <summary>
+    /// Тестирование нахождения списка вакансий
+    /// </summary>
+    [TestClass]
+    public class VacancyFinderTests
+    {
+        private const string TestVacancySearchQuery = "программист";
+
+        /// <summary>
+        /// Тестирование поиска вакансий
+        /// </summary>
+        [TestMethod]
+        public async Task TestSearchVacancies()
+        {
+            var finder = new VacancyFinder();
+
+            await finder.StartAsync(TestVacancySearchQuery);
+
+            Assert.IsTrue(finder.CompletedSuccessfully);
+            Assert.AreNotEqual(0, finder.Vacancies.Count);
+        }
+    }
+}
--- HHVacancies/App.xaml.cs
+++ HHVacancies/App.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
+
+[assembly: InternalsVisibleTo("HHVacancies.Tests")]
 
 namespace HHVacancies
 {
